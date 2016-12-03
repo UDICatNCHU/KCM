f53b8151ff5d5eb563a507794a2f98df2f4e7c06
張泰瑋(Chang Tai Wei) <davidtnfsh@gmail.com>
2016-12-03 15:16:16 +0800

    更新完整版的wiki載點網址

--- langConfig/eng.sh
+++ langConfig/eng.sh
@@ -1,9 +1,9 @@
 mkdir -p WikiRaw/eng;
 # 下載原始的wiki raw data
-cd WikiRaw/bz2; wget https://dumps.wikimedia.org/enwiki/20161120/enwiki-20161120-pages-articles1.xml-p000000010p000030302.bz2;
+cd WikiRaw/bz2; wget https://dumps.wikimedia.org/enwiki/20161201/enwiki-20161201-pages-articles.xml.bz2;
 
 # Extract 維基的XML
-cd ../;python2 WikiPreProcessor.py -wiki bz2/enwiki-20161120-pages-articles1.xml-p000000010p000030302.bz2 -o eng -l=eng
+cd ../;python2 WikiPreProcessor.py -wiki bz2/enwiki-20161201-pages-articles.xml.bz2 -o eng -l=eng
 
 # mv extended jieba dictionary to dictionary directory
 # mv WikiRaw/cht/jieba_expandDict_trad.txt dictionary/
