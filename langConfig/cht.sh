6813b596c6cc866f96244f0aa40a18e0f49ade91
davidtnfsh <davidtnfsh@gmail.com>
2016-12-07 10:41:20 +0800

    先把擴充字典關掉，以確保build過程不出錯

--- langConfig/cht.sh
+++ langConfig/cht.sh
@@ -6,4 +6,4 @@ cd WikiRaw/bz2; wget https://dumps.wikimedia.org/zhwiki/20161120/zhwiki-20161120
 cd ../;python2 WikiPreProcessor.py -wiki bz2/zhwiki-20161120-pages-articles1.xml.bz2 -o cht -l=cht ;python2 WikiPreProcessor.py -wiki bz2/zhwiki-20161120-pages-articles2.xml.bz2 -o cht -l=cht ;python2 WikiPreProcessor.py -wiki bz2/zhwiki-20161120-pages-articles3.xml.bz2 -o cht -l=cht ;python2 WikiPreProcessor.py -wiki bz2/zhwiki-20161120-pages-articles4.xml.bz2 -o cht -l=cht
 
 # mv extended jieba dictionary to dictionary directory
-mv WikiRaw/cht/jieba_expandDict_trad.txt dictionary/
+#mv WikiRaw/cht/jieba_expandDict_trad.txt dictionary/
